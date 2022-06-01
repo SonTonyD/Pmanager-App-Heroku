c5b8dde91a8ad5b6d67fb1f84bdde090a7e40def
sontonyD <sontony.dinh@gmail.com>
2022-06-01 05:54:35 +0200

    suppr bouton inutile et bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,14 @@ import { UserFileComponent } from './user-file/user-file.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './home-page/profile/profile.component';
 import { ViewAllFilesComponent } from './view-all-files/view-all-files.component';
+import { ContentMyPageComponent } from './home-page/content-mypage/content-mypage.component';
 
 const routes : Routes = [
   { path: 'functionality', component: HomeComponent},
   { path: 'signIn', component: SignInComponent},
   { path: 'functionality/user-file', component: ViewAllFilesComponent},
   { path: 'register', component: RegisterComponent},
-  { path: 'profile', component: ProfileComponent},
+  { path: 'profile', component: ContentMyPageComponent},
   { path: '', component: HomePageComponent}
 
 ]
