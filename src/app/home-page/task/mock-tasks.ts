b84a89208f7daf35f6e158d3271f30a12016930d
sontonyD <sontony.dinh@gmail.com>
2022-06-01 06:50:38 +0200

    page accueil

--- src/app/home-page/task/mock-tasks.ts
+++ src/app/home-page/task/mock-tasks.ts
@@ -7,15 +7,15 @@ export const TASKS : Task[] = [
     },
     {
         title : 'Partager',
-        content : "Envoyez rapidement n'importe quel fichier, volumineux ou non, à n'importe qui, même si cette personne ne possède pas de compte Dropbox."
+        content : "Envoyez rapidement n'importe quel fichier, volumineux ou non, à n'importe qui."
     },
     {
         title : 'Protéger',
-        content : "Préservez la confidentialité de vos fichiers avec les différents niveaux de protection offerts par le service auquel des millions de personnes font confiance."
+        content : "Préservez la confidentialité de vos fichiers avec les différents niveaux de protection offerts par le service."
     },
     {
         title : 'Collaborer',
-        content : "Gérez les tâches, suivez les mises à jour de fichiers et restez en phase avec vos équipes et vos clients."
+        content : "Suivez les mises à jour de fichiers et restez en phase avec vos équipes et vos clients."
     }
 
 ]
